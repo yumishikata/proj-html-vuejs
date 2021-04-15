6c11a8493a7d048dc5ac864d0ffbaf814d1ef3a1
Yumi Shikata <yumishika17@gmail.com>
2021-04-15 15:03:00 +0200

    Vue Footer

--- main.js
+++ main.js
@@ -18,4 +18,48 @@ var app = new Vue ({
             this.currentPage = this.nav[index];
         }
     }
+});
+
+var appDue = new Vue ({
+    el: '#footer-vue',
+    data: {
+        liste: [
+            {  
+                type: 'descrizione',
+                title: 'ABOUT ME',
+                paragraph: 'Vivamus suscipit tortor eget felis porttitor volutpat. Nulla quis lorem ut libero malesuada feugiat. Vivamus suscipit tortor eget felis porttitor volupat.'
+            },
+            {  
+                type: 'link',
+                title: 'USEFUL LINKS',
+                list: [
+                    'Latest Books',
+                    'Upcoming Events',
+                    'Recent Articles',
+                    'Business Enquiries',
+                    'Visit My Foundation'
+                ]
+            },
+            {  
+                type: 'contatti',
+                title: 'CONTACT INFO',
+                list: [
+                    '63 E. Studebaker Street Bolingbrook, IL 60440',
+                    'Phone: (800) 000-0000',
+                    'Mobile: (800) 000-0000',
+                    'Email: info@your-company.com'
+                ],
+                icons: [
+                    'fab fa-facebook-f',
+                    'fab fa-twitter',
+                    'fab fa-instagram',
+                    'fab fa-linkedin-in',
+                    'fab fa-youtube'
+                ]
+
+            },
+            
+        ],
+        copyright: '© Copyright 2012 - 2021 | Avada Theme by ThemeFusion | All Rights Reserved | Powered by WordPress'
+    }
 })
